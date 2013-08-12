37300f9b64ea0b1281cc2da9432de0433d38f69d
Gregor Jasny <gjasny@googlemail.com>
2013-08-12 23:55:10 +0200

    Create and destroy of unit test context properly works
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

--- lib/libv4lconvert/libv4lconvert_tests.cpp
+++ lib/libv4lconvert/libv4lconvert_tests.cpp
@@ -17,39 +17,156 @@ void initWithZero(T* t)
 class V4L2Device
 {
 public:
-    int ioctl(unsigned long int request, void *arg)
+    virtual int ioctl(unsigned long int request, void *arg)
     {
     switch (request)
     {
 	case VIDIOC_QUERYCAP:
-	    return ioctlQuerycap(static_cast<v4l2_capability *>(arg));
+	    return ioctlQuerycap(static_cast<v4l2_capability*>(arg));
+
+	case VIDIOC_ENUM_FMT:
+		return ioctlEnumFmt(static_cast<v4l2_fmtdesc*>(arg));
+
+	case VIDIOC_S_STD:
+		return ioctlSStd(static_cast<v4l2_std_id*>(arg));
+
+	case VIDIOC_ENUMINPUT:
+		return ioctlEnuminput(static_cast<v4l2_input*>(arg));
+
+	case VIDIOC_QUERYCTRL:
+		return ioctlQueryctrl(static_cast<v4l2_queryctrl*>(arg));
+
+	case VIDIOC_G_INPUT:
+		return ioctlGInput(static_cast<int*>(arg));
 
 	default:
-	    std::clog << "Unknown ioctl: " << request << std::endl;
+	    std::clog << "Unknown ioctl"
+	    		<< " nr=" << ((request & _IOC_NRMASK) >> _IOC_NRSHIFT) << std::endl;
 	    errno = EINVAL;
 	    return -1;
 	}
     }
 
-    ssize_t read(void *buffer, size_t n)
+    virtual ssize_t read(void *buffer, size_t n)
     {
 	errno = ENXIO;
 	return -1;
     }
     
-    ssize_t write(const void *buffer, size_t n)
+    virtual ssize_t write(const void *buffer, size_t n)
     {
 	errno = ENXIO;
 	return -1;
     }
 
 public:
-    int ioctlQuerycap(v4l2_capability* cap)
+    virtual int ioctlQuerycap(v4l2_capability* cap)
     {
 	errno = EINVAL;
 	return -1;
     }
+
+    virtual int ioctlEnumFmt(v4l2_fmtdesc* desc)
+    {
+    	errno = EINVAL;
+    	return -1;
+    }
+
+    virtual int ioctlSStd(v4l2_std_id* std)
+    {
+    	errno = EINVAL;
+    	return -1;
+    }
+
+    virtual int ioctlEnuminput(v4l2_input* input)
+    {
+    	errno = EINVAL;
+    	return -1;
+    }
+
+    virtual int ioctlQueryctrl(v4l2_queryctrl* ctrl)
+    {
+    	errno = EINVAL;
+    	return -1;
+    }
+
+    virtual int ioctlGInput(int* input)
+    {
+    	errno = EINVAL;
+    	return -1;
+    }
 };
+
+class V4L2LogDevice : public V4L2Device
+{
+public:
+	V4L2LogDevice(std::ostream& output, V4L2Device& worker) : m_output(output), m_worker(worker)
+	{
+	}
+
+	int ioctl(unsigned long int request, void *arg)
+	{
+	    m_output << "ioctl"
+	    		<< " type=" << ((request & _IOC_TYPEMASK) >> _IOC_TYPESHIFT)
+	    		<< " nr=" << ((request & _IOC_NRMASK) >> _IOC_NRSHIFT)
+	    		<< std::endl;
+
+	    return V4L2Device::ioctl(request, arg);
+	}
+
+	int ioctlQuerycap(v4l2_capability* cap)
+	{
+		m_output << "->VIDIOC_QUERYCAP" << std::endl;
+		int ret = m_worker.ioctlQuerycap(cap);
+		m_output << "<-VIDIOC_QUERYCAP" << std::endl;
+		return ret;
+    }
+
+    int ioctlEnumFmt(v4l2_fmtdesc* desc)
+    {
+		m_output << "->VIDIOC_ENUM_FMT" << std::endl;
+		int ret = m_worker.ioctlEnumFmt(desc);
+		m_output << "<-VIDIOC_ENUM_FMT" << std::endl;
+		return ret;
+    }
+
+    int ioctlSStd(v4l2_std_id* std)
+    {
+		m_output << "->VIDIOC_S_STD" << std::endl;
+		int ret = m_worker.ioctlSStd(std);
+		m_output << "<-VIDIOC_S_STD" << std::endl;
+		return ret;
+    }
+
+    int ioctlEnuminput(v4l2_input* input)
+    {
+		m_output << "->VIDIOC_ENUMINPUT" << std::endl;
+		int ret = m_worker.ioctlEnuminput(input);
+		m_output << "<-VIDIOC_ENUMINPUT" << std::endl;
+		return ret;
+    }
+
+    int ioctlQueryctrl(v4l2_queryctrl* ctrl)
+    {
+		m_output << "->VIDIOC_QUERYCTRLT" << std::endl;
+		int ret = m_worker.ioctlQueryctrl(ctrl);
+		m_output << "<-VIDIOC_QUERYCTRL" << std::endl;
+		return ret;
+    }
+
+    int ioctlGInput(int* input)
+    {
+		m_output << "->VIDIOC_G_INPUT" << std::endl;
+		int ret = m_worker.ioctlGInput(input);
+		m_output << "<-VIDIOC_G_INPUT" << std::endl;
+		return ret;
+    }
+
+protected:
+	std::ostream& m_output;
+	V4L2Device& m_worker;
+};
+
 
 class V4L2MockDevice : public V4L2Device
 {
@@ -64,8 +181,7 @@ class V4L2MockDevice : public V4L2Device
 	
 	cap->capabilities = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_READWRITE;
 
-	errno = ENOTTY;
-	return -1;
+	return 0;
     }
 };
 
@@ -130,10 +246,11 @@ V4L2DeviceRegistry::DeviceMap V4L2DeviceRegistry::m_devices;
 TEST(libv4lconvert, colorspace_conversion)
 {
     V4L2MockDevice* pDevice = new V4L2MockDevice;
+    V4L2LogDevice* pLog = new V4L2LogDevice(std::clog, *pDevice);
     V4L2DeviceRegistry registry;
-    registry.registerDevice(42, pDevice);
+    registry.registerDevice(42, pLog);
 
-    v4lconvert_data* pData = ::v4lconvert_create_with_dev_ops(42, pDevice, registry.getDevOps());
+    v4lconvert_data* pData = ::v4lconvert_create_with_dev_ops(42, pLog, registry.getDevOps());
     ASSERT_TRUE(pData);
     
     v4lconvert_destroy(pData); pData = NULL;
